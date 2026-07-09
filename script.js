59515139ffcf74a2ed829371144ee67f93b2119c
a30701-pixel <ima30701@gmail.com>
2026-07-09 20:12:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function startWebsite() {
-    document.querySelector(".welcome").style.display = "none";
-    document.getElementById("main").style.display = "block";
+  const welcome = document.querySelector(".welcome");
+  const main = document.getElementById("main");
+
+  welcome.style.opacity = "0";
+
+  setTimeout(() => {
+    welcome.style.display = "none";
+    main.style.display = "block";
+    main.style.opacity = "0";
+
+    setTimeout(() => {
+      main.style.transition = "opacity 1.5s";
+      main.style.opacity = "1";
+    }, 100);
+
+  }, 800);
 }
