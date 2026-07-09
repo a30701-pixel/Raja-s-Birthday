b55aa74cff07f6f98fb5f0f8edb26089a60dcd6a
a30701-pixel <ima30701@gmail.com>
2026-07-09 20:38:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function startWebsite() {
 
   }, 800);
 }
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.className="heart";
+
+    heart.innerHTML="❤️";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.animationDuration=(3+Math.random()*3)+"s";
+
+    document.getElementById("hearts").appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },6000);
+}
+
+setInterval(createHeart,300);
